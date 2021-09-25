d6bb3fcb900482ae63c35ddf69c714ac0513852c
tknight20 <tknight@westmont.edu>
2021-09-24 17:08:58 -0700

    i hate rust

--- main.rs
+++ main.rs
@@ -1,15 +1,113 @@
 use std::collections::HashMap;
+use std::iter::Enumerate;
+use std::process;
 
 fn lookup(token: &char) -> String{
+ 
     //define the hash map according to the token table
-    let token_map: HashMap<char, &str> = [('=', "ASSIGN"), ('+', "ADD_OP"), ('-', "SUB_OP"), ('*', "MUL_OP"), ('/', "DIV_OP"), (';', "SEMI"), ('(', "OP_PAR"), (')', "CLOSE_PAR")].iter().cloned().collect();
+    let token_map: HashMap<char, &str> = [('=', "ASSIGN"), ('+', "ADD_OP"), ('-', "SUB_OP"), ('*', "MUL_OP"), ('/', "DIV_OP"), (';', "SEMI"), ('(', "OP_PAR"), (')', "CL_PAR")].iter().cloned().collect();
     //return the value that corresponds to the char key, errors out if invalid lexeme is passed in
     token_map.get(&token).expect("Value is not in token hash map").to_string()
 
 }
 
-fn main() {
-    let input_str = "(sum+47)/total";
+fn expr(lexemes: &Vec<String>, tokens: &Vec<String>){
+
+    let mut iterator = tokens.iter().enumerate();
+
+    println!("Enter <expr>");
+
+    for (i, item) in &mut iterator{
+
+        if(item == "ADD_OP" || item == "SUB_OP"){
+
+            println!("Lexeme is: {} Token is: {}", lexemes[i], item);
+            expr(&lexemes[(i+1)..].to_vec(), &tokens[(i+1)..].to_vec());
+            println!("Exit <expr>");
+
+        } else{
+
+            term(&lexemes[i..].to_vec(), &tokens[i..].to_vec());
+
+        }
+
+    }
+
+}
+
+fn term(lexemes: &Vec<String>, tokens: &Vec<String>){
+
+    let mut iterator = tokens.iter().enumerate();
+
+    println!("Enter <term>");
+
+    for (i, item) in &mut iterator{
+
+        if(item == "MUL_OP" || item == "DIV_OP"){
+
+            println!("Lexeme is: {} Token is: {}", lexemes[i], item);
+            expr(&lexemes[(i+1)..].to_vec(), &tokens[(i+1)..].to_vec());
+            println!("Exit <term>");
+
+        } else{
+
+            factor(&lexemes[i..].to_vec(), &tokens[i..].to_vec());
+
+        }
+
+    }
+    println!("Exit <term>");
+
+}
+
+fn factor(lexemes: &Vec<String>, tokens: &Vec<String>){
+
+    let mut iterator = tokens.iter().enumerate();
+
+    println!("Enter <factor>");
+
+    for (i, item) in &mut iterator{
+
+        if(item == "ITY" || item == "INT_LIT"){
+
+            println!("Lexeme is: {} Token is: {}", lexemes[i], item);
+            expr(&lexemes[(i+1)..].to_vec(), &tokens[(i+1)..].to_vec());
+            println!("Exit <factor>");
+
+        } else if(item == "OP_PAR"){
+
+            println!("Lexeme is: {} Token is: {}", lexemes[i], item);
+            expr(&lexemes[(i+1)..].to_vec(), &tokens[(i+1)..].to_vec());
+
+
+        } else if(item == "CL_PAR"){
+
+             println!("Lexeme is: {} Token is: {}", lexemes[i], item);
+             expr(&lexemes[(i+1)..].to_vec(), &tokens[(i+1)..].to_vec());
+        }else if(item == "EOF"){
+
+            process::exit(0);
+
+        }
+
+
+
+        // {
+
+        //     expr(&lexemes[i..].to_vec(), &tokens[i..].to_vec());
+
+        // }
+
+    }
+
+    println!("Exit <factor>");
+
+
+
+}
+
+fn lex(input_str: &str) -> (Vec<String>, Vec<String>) {
+    
     let mut chars = input_str.chars().peekable();
 
     let mut lexemes = Vec::<String>::new();
@@ -47,7 +145,16 @@ fn main() {
             chars.next();
         }
     }
-    
-    println!("{:?}", lexemes);
-    println!("{:?}", tokens);
+    lexemes.push("EOF".to_string());
+    tokens.push("EOF".to_string());
+    (lexemes, tokens)
+}
+
+fn main(){
+
+    let input_str = String::from("(sum+47)/total");
+    let mut lexemes = lex(&input_str).0;
+    let mut tokens = lex(&input_str).1;
+    expr(&lexemes, &tokens);
+
 }
